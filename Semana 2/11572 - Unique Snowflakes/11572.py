86dfb5e148ce427cfc47f43e979863ea50826697
Matheus Araujo <matheusaraujob@dcc.ufrj.br>
2022-01-03 13:41:38 -0300

    Trabalho 11572 Unique Snowflakes accepted

--- Semana 2/11572 - Unique Snowflakes/11572.py
+++ Semana 2/11572 - Unique Snowflakes/11572.py
@@ -1,33 +1,30 @@
-def biggestUniqueStream(stream, i):
-    u = []
-    for i in range(i, len(stream)):
-        if(stream[i] not in u):
-            u.append(stream[i])
-        else:
-            break
-    return len(u)
-
-nTests = int(input())
+nTests = int(input().strip())
 
 for i in range(nTests):
-    nCases = int(input())
+    nCases = int(input().strip())
     flakes = []
-    
+    m = {}
+    maxSize = 0
+    bot = 0
+    cont = 0
     for i in range(nCases):
-        flakes.append(int(input()))
-    
-    size = 0
-    for i in range(len(flakes)):
-        u = []
-        for j in range(i, len(flakes)):
-            if(flakes[j] not in u):
-                u.append(flakes[j])
-            else:
-                break
-        big = len(u)
-        if(size < big):
-            size = big
-    print(size)
-    
-    #print(flakes)
-    #print(a)
+        newFlake = int( input().strip() )
+        #pos = contains(flakes, bot, i, newFlake)
+        flakes.append(newFlake)
+        
+        if(newFlake in m):
+            if(cont > maxSize):
+                maxSize = cont
+                
+            bot = m[newFlake]
+            
+            for j in range(i - cont, bot+1):
+                del m[flakes[j]]
+                cont-=1
+        m[newFlake] = i
+        cont += 1
+        
+        
+    if(cont> maxSize):
+        maxSize = cont
+    print(maxSize)
